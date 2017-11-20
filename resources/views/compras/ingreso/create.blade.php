7fee72e670c0f26c68316eb5a684f532c1d189c7
erickiki <ikiquisbert@gmail.com>
2017-11-20 03:35:31 -0400

    modificando create.blade agregando tabla y configurando lo visual

--- resources/views/compras/ingreso/create.blade.php
+++ resources/views/compras/ingreso/create.blade.php
@@ -54,21 +54,70 @@
             <div class="row">
                   <div class="panel panel-primary">
                         <div class="panel-body">
-                              <div class="col-lg-3 col-sm-3 col-md-3 col-xs-12"> 
-                                    <label>Articulo</label>
-                                    <select name="pidarticulo" class="form-control selectpicker" id="pidarticulo" data-live-search='true'>
-                                          @foreach($articulos as $articulo)
-                                          <option value="{{$articulo->idarticulo}}">{{$articulo->articulo}}</option>
-                                          @endforeach
-                                    </select>
+                              <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12"> 
+                                    <div class="form-group">
+                                          <label>Articulo</label>
+                                          <select name="pidarticulo" class="form-control selectpicker" id="pidarticulo" data-live-search='true'>
+                                                @foreach($articulos as $articulo)
+                                                <option value="{{$articulo->idarticulo}}">{{$articulo->articulo}}</option>
+                                                @endforeach
+                                          </select>
+                                    </div>
+                              </div>
+                              <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                                    <div class="form-group">
+                                          <label for="cantidad">Cantidad</label>      
+                                          <input type="number" name="pcantidad" id="pcantidad" class="form-control" placeholder="cantidad">
+                                    </div>
+                              </div>
+                              <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                                    <div class="form-group">
+                                          <label for="precio_compra">Precio Compra</label>      
+                                          <input type="number" name="pprecio_compra" id="pprecio_compra" class="form-control" placeholder="P. compra">
+                                    </div>
+                              </div>
+                              <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                                    <div class="form-group">
+                                          <label for="precio_venta">Precio Venta</label>      
+                                          <input type="number" name="pprecio_venta" id="pprecio_venta" class="form-control" placeholder="P. venta">
+                                    </div>
+                              </div>      
+                              <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                                    <div class="form-group">
+                                          <button type="button" id="bt_add" class="btn btn-primary">Agregar</button>
+                                    </div>
+                              </div> 
+                              <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                                    <table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
+                                          <thead style="background-color:#A9D0F5">
+                                                <th>Opciones</th>
+                                                <th>Articulos</th>
+                                                <th>Cantidad</th>
+                                                <th>Precio Commpra</th>
+                                                <th>Precio Venta</th>
+                                                <th>Sub-Total</th>
+                                          </thead>      
+                                          <tfoot>
+                                                <th>TOTAL</th>
+                                                <th></th>
+                                                <th></th>
+                                                <th></th>
+                                                <th></th>
+                                                <th><h4 id="total">S/. 0.00</h4></th>
+                                          </tfoot>
+                                          <tbody>
+
+
+                                          </tbody>
+                                    </table>
                               </div>
                         </div>
-
                   </div>
             	<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
             		<div class="form-group">
-	            	<button class="btn btn-primary" type="submit">Guardar</button>
-	            	<button class="btn btn-danger" type="reset">Cancelar</button>
+                              <input name="_token" value="{{ csrf_token()}}" type="hidden"></input>
+      	            	<button class="btn btn-primary" type="submit">Guardar</button>
+      	            	<button class="btn btn-danger" type="reset">Cancelar</button>
             		</div>
             	</div>
             </div>
